a5fd4c19f9d9f26ef1e11a90cbcf8f5e31fe4716
Gana Teja Akula <ganatejaakula@Ganas-MacBook-Pro.local>
2025-08-02 22:13:12 -0400

    removed dead code

--- services/spotify/interface.go
+++ services/spotify/interface.go
@@ -2,7 +2,7 @@ package spotify
 
 import "backend/server/models"
 
-// Service defines the interface for Spotify operations
+// Service defines the Spotify service interface
 type Service interface {
 	GetAccessToken() (string, error)
 	GetTrackByID(trackID string) (*models.SpotifyTrack, error)
